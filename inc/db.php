2995fd29e7414d40ec6bf42baa5c53a804fdb86f
Esteban Luis <jorge.trujillo.luis.esteban@gmail.com>
2019-07-16 11:13:37 +0100

    version 1.2

--- inc/db.php
+++ inc/db.php
@@ -1,6 +1,6 @@
 <?php
 
-  $db = new mysqli('localhost', '31908etrujill4', 'A5pph4%6', '31908etrujill4db');
+  $db = new mysqli('localhost', '', '', '');
   if($db->connect_error) {
     $error = $db->connect_error;
   }
